ab96cd34a69365987c4ea908bc29815e12404f9f
Rohit Gohri <work@rohit.page>
2021-01-19 02:30:27 +0530

    fix plugin options parsing

--- packages/docusaurus-plugin-redoc/src/index.ts
+++ packages/docusaurus-plugin-redoc/src/index.ts
@@ -18,47 +18,49 @@ export default function redocPlugin(
   context: LoadContext,
   opts: PluginOptions,
 ): Plugin<string | null, typeof PluginOptionSchema> {
-  const options: PluginOptions = {...DEFAULT_OPTIONS, ...opts};
   const { baseUrl } = context.siteConfig;
+  const options: PluginOptions = {...DEFAULT_OPTIONS, ...opts};
   const { debug, spec, specUrl } = options;
-
+  if (debug) {
+    console.error('[REDOCUSAURUS_PLUGIN] Opts Input:', opts);
+    console.error('[REDOCUSAURUS_PLUGIN] Options:', options);
+  }
   return {
     name: 'docusaurus-plugin-redoc',
     async loadContent() {
+      let content: any = null;
       if (spec) {
         const file = await fs.readFile(spec);
 
         if (spec.endsWith('.yaml') || spec.endsWith('.yml')) {
           const parsedSpec = YAML.parse(spec);
-          return parsedSpec;
+          content = parsedSpec;
         }
-        return JSON.parse(file.toString());
+        else content = JSON.parse(file.toString());
       }
-      return null;
+      if (debug) {
+        console.error('[REDOCUSAURUS_PLUGIN] Content:', content);
+      }
+      return content;
     },
     async contentLoaded({content, actions}) {
       const {createData, addRoute} = actions;
-      let spec;
+      let specData;
 
       if (content) {
-        spec = await createData(
+        specData = await createData(
           `redocApiSpec-${options.id || '1'}.json`,
           JSON.stringify({ type: 'object', content }),
         );
       }
       else if (specUrl) {
-        spec = await createData(
+        specData = await createData(
           `redocApiSpec-${options.id || '1'}.json`,
           JSON.stringify({ type: 'url', content: specUrl }),
         );
       }
       else {
         console.error('[Redocusaurus] No spec provided');
-        if (debug) {
-          console.error('[PLUGIN_REDOC] Opts Input:', opts);
-          console.error('[PLUGIN_REDOC] Options:', options);
-          console.error('[PLUGIN_REDOC] Content:', content);
-        }
         return;
       }
       const path = options.routePath.startsWith('/') ? options.routePath.slice(1) : options.routePath;
@@ -66,22 +68,25 @@ export default function redocPlugin(
         path: normalizeUrl([baseUrl, path]),
         component: options.apiDocComponent,
         modules: {
-          spec,
+          spec: specData,
         },
         exact: true,
       };
 
       if (debug) {
-        console.error("[PLUGIN_REDOC] Route:", routeOptions);
+        console.error("[REDOCUSAURUS_PLUGIN] Route:", routeOptions);
       }
       addRoute(routeOptions);
     },
   };
 };
 
-export async function validateOptions({
-  validate,
+export function validateOptions({
   options,
 }: OptionValidationContext<PluginOptions, ValidationError>): Promise<void> {
-  await PluginOptionSchema.validateAsync(options);
+  const {value, error} = PluginOptionSchema.validate(options);
+  if (error) {
+    throw error;
+  }
+  return value;
 }
--- packages/docusaurus-plugin-redoc/src/options.ts
+++ packages/docusaurus-plugin-redoc/src/options.ts
@@ -13,12 +13,12 @@ export const DEFAULT_OPTIONS: PluginOptions = {
   // specUrl: 'https://redocly.github.io/redoc/openapi.yaml',
   routePath: '/api/', // URL Route.
   apiDocComponent: '@theme/ApiDoc',
-  debug: true,
+  debug: false,
 };
 
 export const PluginOptionSchema = Joi.object({
   id: Joi.string(),
-  debug: Joi.boolean(),
+  debug: Joi.boolean().default(DEFAULT_OPTIONS.debug),
   spec: Joi.string(),
   specUrl: Joi.string().uri(),
   routePath: Joi.string().default(DEFAULT_OPTIONS.routePath),
