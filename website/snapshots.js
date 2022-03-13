c6263417248b059bda36b359b8521c9aaf1de055
Rohit Gohri <code@rohit.page>
2022-03-13 10:58:40 +0530

    update snapshots

--- website/snapshots.js
+++ website/snapshots.js
@@ -1,12 +1,10 @@
 const urls = [
-  { name: 'Landing Page', path: '/' },
-  { name: 'Docs', path: '/docs' },
-  { name: 'Examples', path: '/examples' },
   {
     name: 'YAML Spec',
     path: '/examples/using-spec-yaml/',
   },
   { name: 'URL Spec', path: '/examples/using-spec-url/' },
+  { name: 'Schema Imports', path: '/docs/guides/schema-imports' },
 ];
 
 module.exports = () => {
