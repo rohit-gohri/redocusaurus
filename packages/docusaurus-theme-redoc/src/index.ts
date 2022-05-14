ea361cf12d67b9ec98ba0e3596e042b9f441db71
Rohit Gohri <code@rohit.page>
2022-05-14 15:45:21 +0530

    fallback tty

--- packages/docusaurus-theme-redoc/src/index.ts
+++ packages/docusaurus-theme-redoc/src/index.ts
@@ -17,6 +17,9 @@ export default function redocTheme(
     configureWebpack(_config, isServer) {
       return {
         resolve: {
+          fallback: {
+            tty: false,
+          },
           alias: {
             process: 'process/browser',
           },
