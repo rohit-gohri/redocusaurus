1d4a474956ca8395fb9963cdaf8bdbfc07a71d27
Sam Yong <samyonggit@gmail.com>
2022-06-13 11:38:36 +0800

    update styles.tsx

--- packages/docusaurus-theme-redoc/src/theme/Redoc/Styles.tsx
+++ packages/docusaurus-theme-redoc/src/theme/Redoc/Styles.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import '../../global';
-import type { AppStore, RedocRawOptions } from 'redoc';
+import type { RedocRawOptions } from 'redoc';
 
 /**
  * Don't hydrate/replace server styles
  * @see https://github.com/facebook/react/issues/10923#issuecomment-338715787
  */
 export function ServerStyles(_props: {
-  specProps: SpecProps,
-  lightThemeOptions: RedocRawOptions,
-  darkThemeOptions: RedocRawOptions,
+  specProps: SpecProps;
+  lightThemeOptions: RedocRawOptions;
+  darkThemeOptions: RedocRawOptions;
 }) {
   return <div className="redocusaurus-styles"></div>;
 }
