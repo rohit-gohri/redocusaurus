e5eecb3c37f97aafbcd67a97f7651844e37ad79a
Rohit Gohri <code@rohit.page>
2022-07-15 22:33:51 +0530

    try hydrating with server styles

--- packages/docusaurus-theme-redoc/src/theme/Redoc/Styles.tsx
+++ packages/docusaurus-theme-redoc/src/theme/Redoc/Styles.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useMemo, useRef, useEffect } from 'react';
 import '../../global';
 import type { RedocRawOptions } from 'redoc';
 
 /**
  * Don't hydrate/replace server styles
- * @see https://github.com/facebook/react/issues/10923#issuecomment-338715787
+ * @see https://github.com/preactjs/preact/issues/2364#issuecomment-966707086
  */
 export function ServerStyles(_props: {
   specProps: SpecProps;
   lightThemeOptions: RedocRawOptions;
   darkThemeOptions: RedocRawOptions;
 }) {
-  return <div className="redocusaurus-styles"></div>;
+  const isFirstRun = useRef(true);
+  useEffect(() => {
+    // initialize once
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+    }
+  }, []);
+
+  const styles = useMemo(() => {
+    if (!isFirstRun) {
+      return '';
+    }
+    return document.getElementsByClassName('redocusaurus-styles')[0].innerHTML;
+  }, []);
+
+  return (
+    <div
+      className="redocusaurus-styles"
+      dangerouslySetInnerHTML={{ __html: styles }}
+    ></div>
+  );
 }
