2fd910ba0a8fea3e5c648658d091d0d7895f7345
Rohit Gohri <code@rohit.page>
2024-03-08 18:20:18 +0100

    separate normal options and preset options

--- packages/docusaurus-plugin-redoc/src/options.ts
+++ packages/docusaurus-plugin-redoc/src/options.ts
@@ -15,12 +15,11 @@ type LayoutProps = {
   };
 };
 
-export interface PluginOptions {
-  id?: string;
-  spec: string;
-  url?: string;
-  route?: string;
-  layout?: LayoutProps;
+/**
+ * Can pass only if directly using plugin.
+ * `redocusaurus` auto populates them
+ */
+export interface PluginDirectUsageOptions {
   debug?: boolean;
   themeId?: string;
   /**
@@ -28,24 +27,35 @@ export interface PluginOptions {
    * @see https://redocly.com/docs/cli/configuration/configuration-file/
    */
   config?: string | RawConfig;
+  layout?: LayoutProps;
+}
+
+export interface PluginOptions extends PluginDirectUsageOptions {
+  id?: string;
+  spec: string;
+  url?: string;
+  route?: string;
 }
 
 export interface PluginOptionsWithDefault extends PluginOptions {
   debug: boolean;
 }
 
-export const DEFAULT_OPTIONS: Omit<PluginOptionsWithDefault, 'spec'> = {
+export const DEFAULT_OPTIONS = {
   layout: {},
   debug: false,
-};
+} satisfies Omit<PluginOptions, 'spec'>;
 
 export const PluginOptionSchema = Joi.object<PluginOptions>({
+  // Direct Usage without redocusaurus preset
   id: Joi.string(),
-  spec: Joi.string(),
-  url: Joi.string().uri({ allowRelative: true }).optional(),
-  layout: Joi.any().default(DEFAULT_OPTIONS.layout),
   debug: Joi.boolean().default(DEFAULT_OPTIONS.debug),
-  route: Joi.string().uri({ relativeOnly: true }).optional(),
   config: Joi.any().optional(),
   themeId: Joi.string().optional(),
+
+  // Basic
+  spec: Joi.string(),
+  url: Joi.string().uri({ allowRelative: true }).optional(),
+  route: Joi.string().uri({ relativeOnly: true }).optional(),
+  layout: Joi.any().default(DEFAULT_OPTIONS.layout),
 });
