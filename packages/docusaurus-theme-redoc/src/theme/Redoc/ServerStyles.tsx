29696f8d83efb76fa07edf54a961e0d38778f67f
Anton Ignatov <a.ignatov@truvity.com>
2024-09-12 13:29:34 +0200

    fixed prefixCssSelectors implementation (fix #377)

--- packages/docusaurus-theme-redoc/src/theme/Redoc/ServerStyles.tsx
+++ packages/docusaurus-theme-redoc/src/theme/Redoc/ServerStyles.tsx
@@ -5,49 +5,27 @@ import { AppStore, Redoc, RedocRawOptions } from 'redoc';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { renderToString } from 'react-dom/server';
 import { ServerStyleSheet } from 'styled-components';
+import postcss from 'postcss';
+import prefixer from 'postcss-prefix-selector';
 
-/**
- * @see https://stackoverflow.com/a/54077142
- */
-const prefixCssSelectors = function (rules: string, className: string) {
-  const classLen = className.length;
-  let char, nextChar, isAt, isIn;
+const prefixCssSelectors = function (css: string, className: string): string {
+  const processor = postcss().use(
+    prefixer({
+      prefix: className,
+    }),
+  );
 
-  // makes sure the className will not concatenate the selector
-  className += ' ';
+  return processor.process(css).css;
+};
 
-  // removes comments
-  rules = rules.replace(/\/\*(?:(?!\*\/)[\s\S])*\*\/|[\r\n\t]+/g, '');
+const renderCss = function (store: AppStore): string {
+  const styleSheet = new ServerStyleSheet();
 
-  // makes sure nextChar will not target a space
-  rules = rules.replace(/}(\s*)@/g, '}@');
-  rules = rules.replace(/}(\s*)}/g, '}}');
+  renderToString(
+    styleSheet.collectStyles(React.createElement(Redoc, { store })),
+  );
 
-  for (let i = 0; i < rules.length - 2; i++) {
-    char = rules[i];
-    nextChar = rules[i + 1];
-
-    if (char === '@' && nextChar !== 'f') isAt = true;
-    if (!isAt && char === '{') isIn = true;
-    if (isIn && char === '}') isIn = false;
-
-    if (
-      !isIn &&
-      nextChar !== '@' &&
-      nextChar !== '}' &&
-      (char === '}' || char === ',' || ((char === '{' || char === ';') && isAt))
-    ) {
-      rules = rules.slice(0, i + 1) + className + rules.slice(i + 1);
-      i += classLen;
-      isAt = false;
-    }
-  }
-
-  // prefix the first select if it is not `@media` and if it is not yet prefixed
-  if (rules.indexOf(className) !== 0 && rules.indexOf('@') !== 0)
-    rules = className + rules;
-
-  return rules;
+  return String(styleSheet.instance);
 };
 
 const LIGHT_MODE_PREFIX = "html:not([data-theme='dark'])";
@@ -63,41 +41,26 @@ export function ServerStyles({
   darkThemeOptions: RedocRawOptions;
 }) {
   const fullUrl = useBaseUrl(specProps.url, { absolute: true });
-  const css = {
-    light: '',
-    dark: '',
-  };
-  const lightSheet = new ServerStyleSheet();
-  const lightStore = new AppStore(specProps.spec, fullUrl, lightThemeOptions);
-  renderToString(
-    lightSheet.collectStyles(React.createElement(Redoc, { store: lightStore })),
-  );
-  const lightStyleTag = lightSheet.getStyleTags();
-  let lightCss = lightStyleTag.slice(lightStyleTag.indexOf('>') + 1);
-  lightCss = lightCss.slice(0, lightCss.indexOf('<style'));
-  css.light = prefixCssSelectors(lightCss, LIGHT_MODE_PREFIX);
 
-  const darkSheet = new ServerStyleSheet();
-  const darkStore = new AppStore(specProps.spec, fullUrl, darkThemeOptions);
-  renderToString(
-    darkSheet.collectStyles(React.createElement(Redoc, { store: darkStore })),
+  const lightCss = prefixCssSelectors(
+    renderCss(new AppStore(specProps.spec, fullUrl, lightThemeOptions)),
+    LIGHT_MODE_PREFIX,
   );
-  const darkStyleTag = darkSheet.getStyleTags();
-  let darkCss = darkStyleTag.slice(darkStyleTag.indexOf('>') + 1);
-  darkCss = darkCss.slice(0, darkCss.indexOf('<style'));
-  css.dark = prefixCssSelectors(darkCss, DARK_MODE_PREFIX).slice(
-    DARK_MODE_PREFIX.length + 1,
+
+  const darkCss = prefixCssSelectors(
+    renderCss(new AppStore(specProps.spec, fullUrl, darkThemeOptions)),
+    DARK_MODE_PREFIX,
   );
 
   return (
     <div className="redocusaurus-styles">
       <style
         key="light-mode-styles"
-        dangerouslySetInnerHTML={{ __html: css.light }}
+        dangerouslySetInnerHTML={{ __html: lightCss }}
       />
       <style
         key="dark-mode-styles"
-        dangerouslySetInnerHTML={{ __html: css.dark }}
+        dangerouslySetInnerHTML={{ __html: darkCss }}
       />
     </div>
   );
