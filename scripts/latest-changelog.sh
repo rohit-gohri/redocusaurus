bb98f1de90bb3f1f617b05794f4c5bc991a11a77
Rohit Gohri <code@rohit.page>
2025-05-30 03:16:10 +0200

    fix changelogs

--- scripts/latest-changelog.sh
+++ scripts/latest-changelog.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-cat $INIT_CWD/CHANGELOG.md | grep -Pzo "## $npm_package_version\s+((.|\n)+?)(?=\n##\s)" | head -n -1 | tail -n +3 > $INIT_CWD/latest_CHANGELOG.md
+cat $PWD/CHANGELOG.md | grep -Pzo "## $npm_package_version\s+((.|\n)+?)(?=\n##\s)" | head -n -1 | tail -n +3 > $PWD/latest_CHANGELOG.md
 
 # Add project changelogs
 if cat $PROJECT_CWD/CHANGELOG.md | grep -Eq "$npm_package_name@$npm_package_version"; then
@@ -11,7 +11,7 @@ else
   echo '' >> $PROJECT_CWD/combined-CHANGELOG.md
   echo "## $npm_package_name@$npm_package_version" >> $PROJECT_CWD/combined-CHANGELOG.md
   echo '' >> $PROJECT_CWD/combined-CHANGELOG.md
-  cat $INIT_CWD/latest_CHANGELOG.md >> $PROJECT_CWD/combined-CHANGELOG.md
+  cat $PWD/latest_CHANGELOG.md >> $PROJECT_CWD/combined-CHANGELOG.md
 fi
 
-rm $INIT_CWD/latest_CHANGELOG.md
+rm $PWD/latest_CHANGELOG.md
